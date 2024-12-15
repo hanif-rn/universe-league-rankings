57c006a8866a06dea28c017c6ebb8cac5d14c890
Hanif Noegroho <hanif.noegroho@mail.utoronto.ca>
2024-12-15 19:51:31 +0700

    Refactor tailwind config to accommodate vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
-    themes: ["lemonade"], // Set the theme here
+    themes: ["lemonade"],
   },
 };
